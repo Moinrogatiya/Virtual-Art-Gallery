48db97fef36c7b87a5a1a9bf33e3cc643e928fa3
moin rogatiya <mmoin1722@gmail.com>
2024-08-15 11:40:45 +0530

    Dotnet_RestApi

---
+++ Dotnet_RestApi/loginService/UserLoginService/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserLoginService.Models
+{
+    public partial class Artist
+    {
+        public int Id { get; set; }
+        public string? About { get; set; }
+
+        public virtual User? IdNavigation { get; set; } = null!;
+    }
+}
